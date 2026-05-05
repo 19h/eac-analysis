ae0604a7fe4c59e473bf89586be8d679861d5189
int <kenan@int.mov>
2026-05-06 00:10:01 +0200

    420a35b5b6e6ec55e509f0212f09d8b891d464182dbac6c0b800df87340be846

--- vm_recovered_source_all_evidence_bundle.py
+++ vm_recovered_source_all_evidence_bundle.py
@@ -51,6 +51,7 @@ DEFAULT_SIDECARS = [
     TRACE_DIR / "vm_native_gap_retdec_batch17.c",
     TRACE_DIR / "vm_native_gap_retdec_batch18.c",
     TRACE_DIR / "vm_native_gap_retdec_batch19.c",
+    TRACE_DIR / "vm_native_gap_retdec_batch20.c",
     TRACE_DIR / "vm_static_only_handler_queue.c",
     TRACE_DIR / "vm_static_only_tier0_handler_models.c",
     TRACE_DIR / "vm_static_only_tier1_handler_models.c",
