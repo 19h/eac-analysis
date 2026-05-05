6f937c16c69c34e9210664849b242fc9e0010978
int <kenan@int.mov>
2026-05-05 14:32:01 +0200

    ce9d5f89d9384d4929214667f4e7fe139f09f08520a971df45677e83ed3714a6

--- vm_recovered_source_harness.c
+++ vm_recovered_source_harness.c
@@ -11,11 +11,27 @@ uintptr_t dispatch_table[360];
 
 static uint64_t unresolved_calls;
 static uint64_t last_unresolved_ip;
+static uint64_t ret_patch_calls;
+static uint64_t last_ret_patch_ip;
+static uint32_t last_ret_patch_ret0;
+static uint32_t last_ret_patch_ret1;
+static uint16_t last_ret_patch_stack_off;
+static uint32_t last_ret_patch_flags;
 
 void vm_unresolved_synthetic_tail(VMState *vm, uint64_t vm_ip) {
     (void)vm;
     unresolved_calls++;
     last_unresolved_ip = vm_ip;
+}
+
+void vm_native_ret_patch_tail(VMState *vm, uint64_t vm_ip, uint32_t ret0, uint32_t ret1, uint16_t stack_off, uint32_t flags) {
+    (void)vm;
+    ret_patch_calls++;
+    last_ret_patch_ip = vm_ip;
+    last_ret_patch_ret0 = ret0;
+    last_ret_patch_ret1 = ret1;
+    last_ret_patch_stack_off = stack_off;
+    last_ret_patch_flags = flags;
 }
 
 void vm_program_sketch(VMState *vm, uint64_t vm_ip);
